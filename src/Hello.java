9117056f72a7c174c96ccb739430f2b2895483a9
Laura Vikanis <vikanis@sipgate.de>
2021-05-19 11:34:58 +0200

    working with variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("HelloWorld");
+
+        int myFirstNumber = (10 + 5)*20;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
     }
 }
